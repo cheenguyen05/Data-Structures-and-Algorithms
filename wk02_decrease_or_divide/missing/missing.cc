a4864296639f6e94034b12f9dd0ab869530b0a0f
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-07 15:57:17 +0300

    solved1

--- wk02_decrease_or_divide/missing/missing.cc
+++ wk02_decrease_or_divide/missing/missing.cc
@@ -6,5 +6,10 @@ int searchSmallestMissingIteration(int* arr, int size){
     //TODO: your code here
     //return missing value OR
     //return NO_VALUE_MISSING if no value is missing
+    for (int i = 0; i < size; i++) {
+        if (arr[i] != i) {
+            return i;  
+        }
+    }
     return NO_VALUE_MISSING;
 }
