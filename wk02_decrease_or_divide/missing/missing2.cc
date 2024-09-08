c9475779a07f807e5fcd316e2f7fa52911b8be1d
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-08 22:27:19 +0300

    final solu

--- wk02_decrease_or_divide/missing/missing2.cc
+++ wk02_decrease_or_divide/missing/missing2.cc
@@ -6,16 +6,27 @@ int searchSmallestMissing(int* A, int left, int right){
     //TODO: your code here
     //return missing value OR
     //return NO_VALUE_MISSING if no value is missing
-    if (left > right) {
-        return left; 
+    if (left == right)
+    {
+        if (A[left] != left + A[0])
+        {
+           return A[left]-1;
+        }
     }
+    else
+    {
+        // Calculate middle index
+        int middle = left + (right - left) / 2;
 
-    int mid = (left + right) / 2;
-
-    if (A[mid] == mid) {
-        return searchSmallestMissing(A, mid + 1, right);
-    } else {
-        return searchSmallestMissing(A, left, mid - 1);
+        // If value at middle index matches the middle, the missing number is on the right
+        if (A[middle] == middle + A[0])
+        {
+           return searchSmallestMissing(A, middle + 1, right);
+        }
+        else if(A[middle] > middle + A[0])
+        {
+            return searchSmallestMissing(A, left, middle);
+        }
     }
     return NO_VALUE_MISSING;
 }
