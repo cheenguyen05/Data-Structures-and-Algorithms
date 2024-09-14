3724e1e34c13f8ff2d33c0160e6d6d27ea40a3b6
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-14 23:04:36 +0300

    fixed

--- wk03_stl/stl/stl5.cc
+++ wk03_stl/stl/stl5.cc
@@ -17,15 +17,20 @@ using namespace std;
  */
 int sortMod3(std::vector<int>& v)
 {
-    // Phân chia vector thành 3 nhóm dựa trên giá trị dư của phép chia cho 3
-    auto candiv3 = std::stable_partition(v.begin(), v.end(), [](int n) { return n % 3 == 0; });
-    auto rem1end = std::stable_partition(candiv3, v.end(), [](int n) { return n % 3 == 1; });
+    try {
 
-    // Sắp xếp từng nhóm theo thứ tự tăng dần
-    std::sort(v.begin(), candiv3);    // Các phần tử chia hết cho 3
-    std::sort(candiv3, rem1end);      // Các phần tử có dư là 1
-    std::sort(rem1end, v.end());        // Các phần tử có dư là 2
+        // Phân chia vector thành 3 nhóm dựa trên giá trị dư của phép chia cho 3
+        auto candiv3 = std::stable_partition(v.begin(), v.end(), [](int n) { return n % 3 == 0; });
+        auto rem1end = std::stable_partition(candiv3, v.end(), [](int n) { return n % 3 == 1; });
 
-    return EXIT_SUCCESS;
+        // Sắp xếp từng nhóm theo thứ tự tăng dần
+        std::sort(v.begin(), candiv3);    // Các phần tử chia hết cho 3
+        std::sort(candiv3, rem1end);      // Các phần tử có dư là 1
+        std::sort(rem1end, v.end());        // Các phần tử có dư là 2
+
+        return EXIT_SUCCESS;
+    } catch (...) {
+        return EXIT_FAILURE;
+    }
 }
 
