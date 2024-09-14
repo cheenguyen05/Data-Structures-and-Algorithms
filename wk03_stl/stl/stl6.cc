754539b0ba0df781b10a02ffba08fe5ffe8eccdc
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-14 17:05:06 +0300

    solved

--- wk03_stl/stl/stl6.cc
+++ wk03_stl/stl/stl6.cc
@@ -17,5 +17,18 @@ using namespace std;
  */
 int findAtLeastGiven(std::map<std::string, int>& m, int given)
 {
+    // Use std::find_if to find the first element that matches the criteria
+    auto it = std::find_if(m.begin(), m.end(), [given](const std::pair<std::string, int>& p) {
+        return p.second >= given;
+    });
+
+    // Check if an element was found
+    if (it != m.end()) {
+        // Return the value of the found element
+        return it->second;
+    } else {
+        // Return NOT_FOUND if no element meets the criteria
+        return NOT_FOUND;
+    }
 }
 
