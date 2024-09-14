010489de727528d6c1a9706955d9df1161f611e6
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-14 23:09:46 +0300

    solved

--- wk03_stl/stl/stl7.cc
+++ wk03_stl/stl/stl7.cc
@@ -16,5 +16,27 @@ using namespace std;
  */
 int findMedian(std::vector<int>& v)
 {
+    // Nếu vector trống, trả về NOT_FOUND
+    if (v.size() == 0) {
+        return NOT_FOUND;
+    }
+    // Sắp xếp vector
+    std::sort(v.begin(), v.end());
+
+    // Tìm median
+    size_t n = v.size();
+    
+    // Nếu số phần tử là số lẻ
+    if (n % 2 == 1) {
+        return v[n / 2];  // Phần tử giữa
+    }
+    
+    // Nếu số phần tử là số chẵn
+    int mid1 = v[n / 2];
+    int mid2 = v[n / 2 - 1];
+    int median = std::floor((mid1 + mid2) / 2.0);  // Làm tròn xuống
+
+    return median;
+
 }
 
