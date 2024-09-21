8982fedc8ec1404a1bfb3ee9d656d24c4b8cae97
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-22 00:11:45 +0300

    finally

--- prg1/datastructures.hh
+++ prg1/datastructures.hh
@@ -1,13 +1,10 @@
-// Datastructures.hh
-
 #ifndef DATASTRUCTURES_HH
 #define DATASTRUCTURES_HH
 
 #include "customtypes.hh"
-#include <utility>
+#include <unordered_map>
 #include <vector>
-
-// Add your own STL includes below this comment
+#include <string>
 
 template <typename Type>
 Type random_in_range(Type start, Type end);
@@ -20,103 +17,47 @@ public:
   Datastructures();
   ~Datastructures();
 
-  // Estimate of performance:
-  // Short rationale for estimate:
   unsigned int get_bite_count();
-
-  // Estimate of performance:
-  // Short rationale for estimate:
   void clear_all();
-
-  // Estimate of performance:
-  // Short rationale for estimate:
   std::vector<BiteID> all_bites();
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  bool add_bite(BiteID /*id*/, const Name & /*name*/, Coord /*xy*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  Name get_bite_name(BiteID /*id*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  Coord get_bite_coord(BiteID /*id*/);
-
-  // We recommend you implement the operations below only after implementing the
-  // ones above
-
-  // Estimate of performance:
-  // Short rationale for estimate:
+  bool add_bite(BiteID id, const Name &name, Coord xy);
+  Name get_bite_name(BiteID id);
+  Coord get_bite_coord(BiteID id);
+  
   std::vector<BiteID> get_bites_alphabetically();
-
-  // Estimate of performance:
-  // Short rationale for estimate:
   std::vector<BiteID> get_bites_distance_increasing();
+  BiteID find_bite_with_coord(Coord xy);
+  bool change_bite_coord(BiteID id, Coord newcoord);
 
-  // Estimate of performance:
-  // Short rationale for estimate:
-  BiteID find_bite_with_coord(Coord /*xy*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  bool change_bite_coord(BiteID /*id*/, Coord /*newcoord*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  bool add_contour(ContourID /*id*/, const Name & /*name*/, ContourHeight /*height*/,
-                   std::vector<Coord> /*coords*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
+  bool add_contour(ContourID id, const Name &name, ContourHeight height, std::vector<Coord> coords);
   std::vector<ContourID> all_contours();
+  Name get_contour_name(ContourID id);
+  std::vector<Coord> get_contour_coords(ContourID id);
+  ContourHeight get_contour_height(ContourID id);
+  bool add_subcontour_to_contour(ContourID id, ContourID parentid);
+  bool add_bite_to_contour(BiteID biteid, ContourID parentid);
+  std::vector<ContourID> get_bite_in_contours(BiteID id);
 
-  // Estimate of performance:
-  // Short rationale for estimate:
-  Name get_contour_name(ContourID /*id*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  std::vector<Coord> get_contour_coords(ContourID /*id*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  ContourHeight get_contour_height(ContourID /*id*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  bool add_subcontour_to_contour(ContourID /*id*/, ContourID /*parentid*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  bool add_bite_to_contour(BiteID biteid, ContourID /*parentid*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  std::vector<ContourID> get_bite_in_contours(BiteID /*id*/);
-
-  // Non-compulsory operations
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  std::vector<ContourID> all_subcontours_of_contour(ContourID /*id*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  std::vector<BiteID> get_bites_closest_to(Coord /*xy*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  bool remove_bite(BiteID /*id*/);
-
-  // Estimate of performance:
-  // Short rationale for estimate:
-  ContourID get_closest_common_ancestor_of_contours(ContourID /*id1*/,
-                                                    ContourID /*id2*/);
+  std::vector<ContourID> all_subcontours_of_contour(ContourID id);
+  std::vector<BiteID> get_bites_closest_to(Coord xy);
+  bool remove_bite(BiteID id);
+  ContourID get_closest_common_ancestor_of_contours(ContourID id1, ContourID id2);
 
 private:
-  // Add stuff needed for your class implementation here
+  struct Bite {
+    Name name;
+    Coord coord;
+  };
+
+  struct Contour {
+    Name name;
+    ContourHeight height;
+    std::vector<Coord> coords;
+  };
+
+  std::unordered_map<BiteID, Bite> bites_;
+  std::unordered_map<ContourID, Contour> contours_;
 };
 
 #endif // DATASTRUCTURES_HH
+
