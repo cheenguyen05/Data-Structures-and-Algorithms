213b6641adc33a8ab5ea5030f316273ef9159eda
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-21 23:34:15 +0300

    abc

--- prg1/datastructures.cc
+++ prg1/datastructures.cc
@@ -1,79 +1,156 @@
+/**
+ * @brief The Datastructures class
+ * STUDENTS: Modify the code below to implement the functionality of the class.
+ * Also remove comments from the parameter names when you implement an operation
+ * (Commenting out parameter name prevents compiler from warning about unused
+ * parameters on operations you haven't yet implemented.)
+ */
+
 #include "datastructures.hh"
 #include "customtypes.hh"
-#include <algorithm>
-#include <stdexcept>
 
 Datastructures::Datastructures()
 {
-    // Constructor: nothing special required here.
+  // Write any initialization you need here
 }
 
 Datastructures::~Datastructures()
 {
-    // Destructor: the unordered_map will clean itself up.
+  // Write any cleanup you need here
 }
 
 unsigned int Datastructures::get_bite_count()
 {
-    return bites_.size();
+  // Replace the line below with your implementation
+  throw NotImplemented("get_bite_count");
 }
 
 void Datastructures::clear_all()
 {
-    bites_.clear();
-    contours_.clear();
+  // Replace the line below with your implementation
+  throw NotImplemented("clear_all");
 }
 
 std::vector<BiteID> Datastructures::all_bites()
 {
-    std::vector<BiteID> bite_ids;
-    for (const auto& [id, bite] : bites_) {
-        bite_ids.push_back(id);
-    }
-    return bite_ids;
+  // Replace the line below with your implementation
+  throw NotImplemented("all_bites");
 }
 
-bool Datastructures::add_bite(BiteID id, const Name &name, Coord xy)
+bool Datastructures::add_bite(BiteID /*id*/, const Name & /*name*/, Coord /*xy*/)
 {
-    if (bites_.find(id) != bites_.end()) {
-        return false; // Bite with this ID already exists
-    }
-    bites_[id] = Bite{name, xy};
-    return true;
+  // Replace the line below with your implementation
+  throw NotImplemented("add_bite");
 }
 
-Name Datastructures::get_bite_name(BiteID id)
+Name Datastructures::get_bite_name(BiteID /*id*/)
 {
-    if (bites_.find(id) != bites_.end()) {
-        return bites_.at(id).name;
-    }
-    throw std::out_of_range("Bite ID not found");
+  // Replace the line below with your implementation
+  throw NotImplemented("get_bite_name");
 }
 
-Coord Datastructures::get_bite_coord(BiteID id)
+Coord Datastructures::get_bite_coord(BiteID /*id*/)
 {
-    if (bites_.find(id) != bites_.end()) {
-        return bites_.at(id).coord;
-    }
-    throw std::out_of_range("Bite ID not found");
+  // Replace the line below with your implementation
+  throw NotImplemented("get_bite_coord");
 }
 
 std::vector<BiteID> Datastructures::get_bites_alphabetically()
 {
-    std::vector<BiteID> bite_ids = all_bites();
-    std::sort(bite_ids.begin(), bite_ids.end(), [this](BiteID a, BiteID b) {
-        return bites_[a].name < bites_[b].name;
-    });
-    return bite_ids;
+  // Replace the line below with your implementation
+  throw NotImplemented("get_bites_alphabetically");
 }
 
 std::vector<BiteID> Datastructures::get_bites_distance_increasing()
 {
-    std::vector<BiteID> bite_ids = all_bites();
-    std::sort(bite_ids.begin(), bite_ids.end(), [this](BiteID a, BiteID b) {
-        auto dist_a = bites_[a].coord.x * bites_[a].coord.x + bites_[a].coord.y * bites_[a].coord.y;
-        auto dist_b = bites_[b].coord.x * bites_[b].coord.x + bites_[b].coord.y * bites_[b].coord.y;
-        return dist_a < dist_b;
-    });
-    return bite_ids;
+  // Replace the line below with your implementation
+  throw NotImplemented("get_bites_distance_increasing");
+}
+
+BiteID Datastructures::find_bite_with_coord(Coord /*xy*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("find_bite_with_coord");
+}
+
+bool Datastructures::change_bite_coord(BiteID /*id*/, Coord /*newcoord*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("change_bite_coord");
+}
+
+bool Datastructures::add_contour(ContourID /*id*/, const Name & /*name*/, ContourHeight /*height*/,
+                                 std::vector<Coord> /*coords*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("add_contour");
+}
+
+std::vector<ContourID> Datastructures::all_contours()
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("all_contours");
+}
+
+Name Datastructures::get_contour_name(ContourID /*id*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("get_contour_name");
+}
+
+std::vector<Coord> Datastructures::get_contour_coords(ContourID /*id*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("get_contour_coords");
+}
+
+ContourHeight Datastructures::get_contour_height(ContourID /*id*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("get_contour_height");
+}
+
+bool Datastructures::add_subcontour_to_contour(ContourID /*id*/,
+                                               ContourID /*parentid*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("add_subcontour_to_contour");
+}
+
+bool Datastructures::add_bite_to_contour(BiteID /*biteid*/, ContourID /*contourid*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("add_bite_to_contour");
+}
+
+std::vector<ContourID> Datastructures::get_bite_in_contours(BiteID /*id*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("get_bite_in_contours");
+}
+
+std::vector<ContourID>
+Datastructures::all_subcontours_of_contour(ContourID /*id*/)
+{
+  throw NotImplemented("all_subcontours_of_contour");
+}
+
+ContourID
+Datastructures::get_closest_common_ancestor_of_contours(ContourID /*id1*/,
+                                                        ContourID /*id2*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("get_closest_common_ancestor_of_contours");
+}
+
+bool Datastructures::remove_bite(BiteID /*id*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("remove_bite");
+}
+
+std::vector<BiteID> Datastructures::get_bites_closest_to(Coord /*xy*/)
+{
+  // Replace the line below with your implementation
+  throw NotImplemented("get_bites_closest_to");
 }
