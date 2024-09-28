dce7e64c6bb6d5a8169f89b73b52f65d1f2550f2
Chi Nguyen <chi.h.nguyen@tuni.fi>
2024-09-28 23:24:32 +0300

    more effective

--- wk05_graphs/improving_functions/improve1.cc
+++ wk05_graphs/improving_functions/improve1.cc
@@ -10,9 +10,9 @@
  * @return std::vector<int>
  */
 std::vector<int> ascendingVector(int n){
-    std::vector<int> v;
+    std::vector<int> v(n); //preallocattte memory for n elements
     for ( int i = 0; i < n ; i++){
-        v.insert(v.end(), i);
+        v[i] = i;//assign values directly
     }
     return v;
 }
